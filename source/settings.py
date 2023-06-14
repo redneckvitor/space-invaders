cba602332112def65292320b7caa471fcb264d84
steingames <joaovitorpessoal@protonmail.com>
2023-06-14 15:03:09 -0300

    Files added
    Project setup

---
+++ source/settings.py
@@ -0,0 +1,54 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initializes game's static settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 4.5
+        self.ship_limit = 3
+
+        # Bullet settings
+        self.bullet_speed = 220.0
+        self.bullet_width = 300
+        self.bullet_height = 20
+        self.bullet_color = (255, 60, 60)
+        self.bullets_allowed = 4
+
+        # Alien settings
+        self.alien_speed = 10.0
+        self.fleet_drop_speed = 50 #20
+        # fleet direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 10.5
+        self.bullet_speed = 10.0
+        self.alien_speed = 2.0
+
+        # Fleet direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+        # Scoring settings
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """increase speed settings and alien point values."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points + self.score_scale)
+
